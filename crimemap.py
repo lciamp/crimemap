6f149610a57f4975bf0e597fde80ad5c4c4fec5c
Louis Ciampanelli <louisciampanelli@Louiss-MBP.home>
2017-07-31 16:16:38 -0400

    crimemap

---
+++ crimemap.py
@@ -0,0 +1,52 @@
+from dbhelper import DBHelper
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+DB = DBHelper()
+
+# index
+@app.route("/")
+def home():
+    try:
+        data = DB.get_all_inputs()
+    except Exception as e:
+        print e
+        data = None
+    return render_template("home.html", data=data)
+
+# add to database
+@app.route("/add", methods=["POST"])
+def add():
+    try:
+        data = request.form.get("userinput")
+        DB.add_input(data)
+    except Exception as e:
+        print e
+    return home()
+
+# clear database
+@app.route("/clear")
+def clear():
+    try:
+        DB.clear_all()
+    except Exception as e:
+        print e
+    return home()
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
