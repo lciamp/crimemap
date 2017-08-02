47cde78ddf8cd30f48d9a4e59fd1ec5a49c7b882
Louis Ciampanelli <louisciampanelli@Louiss-MBP.home>
2017-08-01 21:34:38 -0400

    added validation

--- crimemap.py
+++ crimemap.py
@@ -1,4 +1,4 @@
-import json
+import dateparser, datetime, json, string
 from dbhelper import DBHelper
 from flask import Flask, render_template, request
 
@@ -8,14 +8,12 @@ categories = ['Mugging', 'Break-in', 'Robbery', 'Shooting']
 
 # index
 @app.route("/")
-def home():
-    try:
-        crimes = DB.get_all_crimes()
-        crimes = json.dumps(crimes)
-    except Exception as e:
-        print e
-        crimes = None
-    return render_template("home.html", crimes=crimes, categories=categories)
+def home(error_message=None):
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template("home.html", crimes=crimes, 
+                                        categories=categories,
+                                        error_message=error_message)
 
 # add to database
 @app.route("/add", methods=["POST"])
@@ -43,16 +41,41 @@ def submitcrime():
         if category not in categories:
             return home()
         date = request.form.get('date')
-        latitude = request.form.get('latitude')
-        longitude = request.form.get('longitude')
-        description = request.form.get('description')
+        if not date:
+            return home("Invalid date. Please use yyyy-mm-dd format.")
+        try:
+            latitude = float(request.form.get('latitude'))
+            longitude = float(request.form.get('longitude'))
+        except ValueError:
+            return home()
+        description = sanitize_string(request.form.get('description'))
         DB.add_crime(category, date, latitude, longitude, description)
     except Exception as e:
         print e
     return home()
 
-
-
+# run the app
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
+# HELPER FUNCTIONS
+# function for formating the date if input by user 
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+# function for sanitizing description text
+def sanitize_string(userinput):
+    whitelist = string.letters + string.numbers + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
+
+
+
+
+
+
+
 
